595e795688ddff5e856edc8b91a6df3ca6de7f23
anbuKakashi <kakafuad12@gmail.com>
2018-11-28 09:52:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world coyyyyy selamat pagi\n');
+  res.send('Hello world coyyyyy selamat pagi bos\n');
 });
 
 app.listen(PORT, HOST);
